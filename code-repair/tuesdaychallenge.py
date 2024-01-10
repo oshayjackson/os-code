f5883048623f17419aa812371d8d377c20ce8c7b
Oshay Jackson <oshay.py@gmail.com>
2024-01-10 08:03:32 -0800

    tues code-repair

--- code-repair/tuesdaychallenge.py
+++ code-repair/tuesdaychallenge.py
@@ -6,9 +6,13 @@ mylist = [1, 2, 3, 4, 5, "Python"]
 def hello():
     # name= input(What is your name?\n>)
     name = input("What is your name?\n")
-    return f"Hi {name.capitalize()}! Welcome to the Day {mylist[1]} of {mylist[5]}Python Training!"
+
+    return print(
+        f"Hi {name.capitalize()}! Welcome to the Day {mylist[1]} of {mylist[5]}Python Training!"
+    )
 
 
+hello()
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
 # print("Hi " + name.capitalize + "! Welcome to Day " + mylist[1] + " of " + mylist[6] " Training!")
